381bf6b97c207063d9837b8b7a378dab683cfb42
AVP-ST <v.papic@alumnos.santotomas.cl>
2023-09-12 15:24:22 -0300

    v1.2 app bazar images included

--- app/src/main/java/com/example/bazar_santotomas/MainActivity.java
+++ app/src/main/java/com/example/bazar_santotomas/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.bazar_santotomas;
 
+import android.content.Intent;
+import android.view.View;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickAcceder(View view)
+    {
+        Intent intent = new Intent(this,Acceder.class);
+        startActivity(intent);
     }
 }
