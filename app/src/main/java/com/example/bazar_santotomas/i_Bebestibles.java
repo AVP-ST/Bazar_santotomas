8fb96751c131dd62907241b8963c0865f894720f
Cristobalt8 <j.toledo30@alumnos.santotomas.cl>
2023-09-14 01:42:44 -0300

    Cargando nuevas vistas de inventarios y con adicional de boton en actividades

---
+++ app/src/main/java/com/example/bazar_santotomas/i_Bebestibles.java
@@ -0,0 +1,16 @@
+package com.example.bazar_santotomas;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class i_Bebestibles extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invt_bebestibles);
+    }
+
+
+
+}
